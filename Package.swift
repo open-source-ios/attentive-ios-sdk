6aca0867b007c3c2374f22b6ca218ba861f196fa
Alok Choudhary <alok.c83@gmail.com>
2023-09-20 13:27:13 -0500

    Package.swift added

---
+++ Package.swift
@@ -0,0 +1,30 @@
+// swift-tools-version: 5.8
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "attentive-ios-sdk",
+    platforms: [
+        .iOS(.v15)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "attentive-ios-sdk",
+            targets: ["attentive-ios-sdk"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "attentive-ios-sdk",
+            dependencies: [],
+            path: "Sources"
+            ),
+        .testTarget(
+            name: "attentive-ios-sdkTests",
+            dependencies: ["attentive-ios-sdk"],
+            path: "Tests"),
+    ]
+)
